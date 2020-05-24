976bfd6a6437075f1b84622161dd86b28d65e952
Albert <albertanchiu@gmail.com>
2020-05-23 23:43:18 -0700

    Make the dummy handler even dumber

--- handlers/appointment.py
+++ handlers/appointment.py
@@ -1,20 +1,9 @@
-import os
-import logging
-import jsonpickle
-import boto3
-from aws_xray_sdk.core import xray_recorder
-from aws_xray_sdk.core import patch_all
+import json
 
-logger = logging.getLogger()
-logger.setLevel(logging.INFO)
-patch_all()
+def handler(event, context):
+    # TODO implement
+    return {
+        'statusCode': 200,
+        'body': json.dumps('Hello from Lambda!')
+    }
 
-client = boto3.client('lambda')
-client.get_account_settings()
-
-def lambda_handler(event, context):
-    logger.info('## ENVIRONMENT VARIABLES\r' + jsonpickle.encode(dict(**os.environ)))
-    logger.info('## EVENT\r' + jsonpickle.encode(event))
-    logger.info('## CONTEXT\r' + jsonpickle.encode(context))
-    response = client.get_account_settings()
-    return response['AccountUsage']
