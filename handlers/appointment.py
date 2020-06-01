73c86e89c3176f1c45db0ccb8c2aec4752689014
Albert <albertanchiu@gmail.com>
2020-06-01 00:52:50 -0700

    Adding check in handler

--- handlers/appointment.py
+++ handlers/appointment.py
@@ -1,6 +1,8 @@
 import json
+import time
+from geopy import distance
 
-appointments = [{"id": "guid",
+appointments = [{"appointment_id": "guid",
                  "name": "Brian",
                  "status": "SCHEDULED",
                  "appointment_time": "1587791538037",
@@ -10,6 +12,21 @@ appointments = [{"id": "guid",
                  }]
 
 def handler(event, context):
-    appointment_id = event['id']
-    appointment = next((ap for ap in appointments if ap["id"] == appointment_id), None);
+    appointment_id = event['appointment_id']
+    appointment = next((ap for ap in appointments if ap["appointment_id"] == appointment_id), None);
+    return appointment
+
+def check_in_handler(event, context):
+    appointment_id = event['appointment_id']
+    appointment = next((ap for ap in appointments if ap["appointment_id"] == appointment_id), None);
+    if not appointment:
+        raise RuntimeError("Appointment not found.")
+    patient_location = (event["latitude"], event["longitude"])
+    dr_location = (appointment["lat"], appointment["long"])
+    dist = distance.distance(patient_location, dr_location).km
+    if dist > 1:
+        raise RuntimeError("Distance of " + str(dist) + " is greater than 1 km, check in not possible.")
+    appointment["status"] = "FILLING_FORMS"
+    appointment["patient_location"] = patient_location
+    appointment["check_in_time"] = int(time.time() * 1000)
     return appointment
