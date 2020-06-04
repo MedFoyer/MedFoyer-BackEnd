87fa4a03ddec773f5226f553fa427d6b183622a1
Albert <albertanchiu@gmail.com>
2020-06-03 17:07:13 -0700

    S3 persistence for submit form

--- handlers/appointment.py
+++ handlers/appointment.py
@@ -1,10 +1,12 @@
 import json
 import time
 import boto3
+import uuid
 from geopy import distance
 
 dynamodb = boto3.resource('dynamodb')
 appointments_table = dynamodb.Table('SANDBOX_APPOINTMENTS')
+s3_client = boto3.client('s3')
 
 def get_appointment(appointment_id):
     dynamo_response = appointments_table.get_item(Key={"appointment_id" : appointment_id})
@@ -50,7 +52,9 @@ true_values = frozenset(["yes", "1", "2", "3", "4", "true", True])
 def submit_form_handler(event, context):
     appointment_id = event['appointment_id']
     form = json.loads(event["form"])
-    get_appointment(appointment_id)
+    appointment = get_appointment(appointment_id)
+    form_id = str(uuid.uuid4())
+    s3_client.put_object(Bucket="sandbox-forms", Key=form_id, Body=json.dumps(form).encode("UTF-8"))
     if not appointment:
         return ("Appointment not found.", 404)
     covid_flag = "NORMAL"
@@ -59,15 +63,17 @@ def submit_form_handler(event, context):
             covid_flag = "AT_RISK"
 
     appointment["covid_flag"] = covid_flag
-    #appointment["form"] = form
-    #appointment["submitted_form_ids"]
+    if "submitted_form_metadata" not in appointment:
+        appointment["submitted_form_metadata"] = []
+    appointment["submitted_form_metadata"].append({"form_id" : form_id, "form_type_id" : "COVID", "form_type_version" : "0"})
     appointment["status"] = "CHECKED_IN"
+    appointments_table.put_item(Item=appointment)
     return appointment
 
 
 def summon_patient_handler(event, context):
     appointment_id = event['appointment_id']
-    get_appointment(appointment_id)
+    appointment = get_appointment(appointment_id)
     if not appointment:
         return ("Appointment not found.", 404)
     if args.special_instructions:
