48158603ddbff04ecfa53d4e9d54ae55bb5c1222
Brian Donald <contact@noimbrian.com>
2020-06-10 17:43:00 -0500

    Add twilio example code

---
+++ handlers/integrations/twilio.py
@@ -0,0 +1,31 @@
+import os
+from twilio.rest import Client
+
+account = os.getenv("twilio_account_sid")
+auth_token = os.getenv("twilio_auth_token")
+client = Client(account, auth_token)
+
+
+def send_message(message, destination):
+    client.messages.create(
+        body=message,
+        from_="+1 833 531 0351",
+        to=destination
+    )
+
+
+def notify_for_appointment(appointment_id):
+    # TODO: Get Patient Phone Number
+    patient_number = "9703104636"  # Brian's number for now
+    send_message(
+        """
+You have an upcoming appointment, and your clinic uses MedFoyer to ensure a safe and smooth check-in.
+
+Please open this link to check-in remotely when you arrive in the parking lot.
+
+If you do not have a smartphone, please reply "NO".
+        
+https://medfoyer.com/patient/appt/%s
+        """ % appointment_id,
+        patient_number
+    )
