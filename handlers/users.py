4576b4187f3dcb70e3d97b0a90e203efea1f7a55
Albert <albertanchiu@gmail.com>
2020-06-22 15:51:57 -0700

    Add admin roles to template

--- handlers/users.py
+++ handlers/users.py
@@ -4,6 +4,12 @@ import os
 cognito_client = boto3.client('cognito-idp')
 user_pool_id = os.environ.get("USER_POOL_ID")
 
+def is_admin(username):
+    response = cognito_client.admin_list_groups_for_user(Username=username,
+                                              UserPoolId=user_pool_id)
+    groups = response["Groups"]
+    return any(group["GroupName"] == "sys-admin" for group in groups)
+
 def create_user_handler(event, context):
     username = event["username"]
     email = event["email"]
@@ -24,7 +30,12 @@ def create_user_handler(event, context):
     except cognito_client.exceptions.UsernameExistsException:
         raise RuntimeError(f"Username {username} already exists!")
 
-    return {"username" : response["User"]["Username"]}
+    attributes = response["User"]["Attributes"]
+    email_attribute = next((attr for attr in attributes if attr["Name"] == "email"), None)
+
+    return {"username" : response["User"]["Username"],
+            "email" : email_attribute["Value"],
+            "is_admin" : False}
 
 def delete_user_handler(event, context):
     username = event["username"]
@@ -34,10 +45,54 @@ def delete_user_handler(event, context):
                                                  Username=username)
         attributes = response["UserAttributes"]
         clinic_id_attribute = next((attr for attr in attributes if attr["Name"] == "custom:clinic_id"), None)
+        email_attribute = next((attr for attr in attributes if attr["Name"] == "email"), None)
+        admin_status = is_admin(username)
         if clinic_id_attribute["Value"] != clinic_id:
             raise RuntimeError(f"Username {username} doesn't exists!")
         cognito_client.admin_delete_user(UserPoolId=user_pool_id,
                                          Username=username)
-        return {"username" : response["Username"]}
+        return {"username" : response["Username"],
+                "email" : email_attribute["Value"],
+                "is_admin" : admin_status}
+    except cognito_client.exceptions.UserNotFoundException:
+        raise RuntimeError(f"Username {username} doesn't exists!")
+
+def make_user_sys_admin_handler(event, context):
+    username = event["username"]
+    clinic_id = event["clinic_id"]
+    try:
+        response = cognito_client.admin_get_user(UserPoolId=user_pool_id,
+                                                 Username=username)
+        attributes = response["UserAttributes"]
+        clinic_id_attribute = next((attr for attr in attributes if attr["Name"] == "custom:clinic_id"), None)
+        email_attribute = next((attr for attr in attributes if attr["Name"] == "email"), None)
+        if clinic_id_attribute["Value"] != clinic_id:
+            raise RuntimeError(f"Username {username} doesn't exists!")
+        cognito_client.admin_add_user_to_group(UserPoolId=user_pool_id,
+                                               Username=username,
+                                               GroupName='sys-admin')
+        return {"username" : response["Username"],
+                "email" : email_attribute["Value"],
+                "is_admin" : True}
+    except cognito_client.exceptions.UserNotFoundException:
+        raise RuntimeError(f"Username {username} doesn't exists!")
+
+def remove_user_from_sys_admins_handler(event, context):
+    username = event["username"]
+    clinic_id = event["clinic_id"]
+    try:
+        response = cognito_client.admin_get_user(UserPoolId=user_pool_id,
+                                                 Username=username)
+        attributes = response["UserAttributes"]
+        clinic_id_attribute = next((attr for attr in attributes if attr["Name"] == "custom:clinic_id"), None)
+        email_attribute = next((attr for attr in attributes if attr["Name"] == "email"), None)
+        if clinic_id_attribute["Value"] != clinic_id:
+            raise RuntimeError(f"Username {username} doesn't exists!")
+        cognito_client.admin_remove_user_from_group(UserPoolId=user_pool_id,
+                                               Username=username,
+                                               GroupName='sys-admin')
+        return {"username" : response["Username"],
+                "email" : email_attribute["Value"],
+                "is_admin" : False}
     except cognito_client.exceptions.UserNotFoundException:
         raise RuntimeError(f"Username {username} doesn't exists!")
