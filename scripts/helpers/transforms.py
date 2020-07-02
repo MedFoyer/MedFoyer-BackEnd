e1a7b4295b6b489db6975d6519ea5c11a281cbe4
Albert <albertanchiu@gmail.com>
2020-07-01 17:13:22 -0700

    add warning message comment on primary key changes

--- scripts/helpers/transforms.py
+++ scripts/helpers/transforms.py
@@ -1,3 +1,6 @@
+#WARNING: Don't change primary keys in any transform.
+# If you do, it will cause new records to be created without any change to the old ones
+
 def combine_names(item):
     item["name"] = item["given_name"] + " " + item["last_name"]
     item.pop("given_name")
